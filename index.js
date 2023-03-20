1df2f02cf5c5ba800141688c34b1894cfd9441eb
brianrono <brianrono16@gmail.com>
2023-03-20 17:13:23 +0300

    comments

--- index.js
+++ index.js
@@ -8,3 +8,5 @@ const totalBatteries = batteryBatches.reduce((accumulator, currentValue) => accu
 // the callback function takes two arguments: the accumulator and the current value being processed
 // in this case, we're simply adding the current value to the accumulator.
 // the second argument to reduce() is an initial value for the accumulator, which we've set to 0
+// when we call reduce() on the batteryBatches array with this callback function and initial value, it will iterate over each element of the array and add it to the accumulator, which starts at 0. 
+// the final value of the accumulator will be the sum of all the elements in the array, which we assign to the variable totalBatteries.
